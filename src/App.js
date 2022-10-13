0c78bc7f29f5d6ccb20900f62cd12d163e56796b
github-aliceyang <aliyang47@gmail.com>
2022-10-13 12:19:12 -0700

    created the repository

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Intro from './components/Intro';
+import TaskFlow from './components/TaskFlow';
+
+
+
+export default function App() {
+  return (
+    <div>
+      <div>
+        <Intro />
+        <TaskFlow />
+   </div>
+    </div>
+  );
+}
+
+
