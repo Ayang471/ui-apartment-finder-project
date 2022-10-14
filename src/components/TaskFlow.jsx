6f1db037928088b81f4f48c1558b468fe436319c
github-aliceyang <aliyang47@gmail.com>
2022-10-14 11:24:56 -0700

    created task flow file

---
+++ src/components/TaskFlow.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import taskflow from "../assets/Task Flow.png";
+import Wireframes from "./Wireframes";
+
+export default function TaskFlow() {
+  return (
+    <div>
+      <div className="w-full h-screen bg-[#0a192f]">
+        <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+          <h1 className="text-3xl sm:text-4xl font-medium text-[#ccd6f6]">
+            User's Task Flow
+          </h1>
+          <p className="text-gray-300 py-4 max-w-700px">
+            A user wants to search for an apartment in a specific area, refine
+            the results by a set of criteria, view the details about an
+            apartment, and initiate contact with the owner.
+          </p>
+          <img src={taskflow} alt="task-flow" />
+        </div>
+        <Wireframes />
+      </div>
+    </div>
+  );
+}
