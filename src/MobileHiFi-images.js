3469893c08e61f7213afde5d3b7742bb8884268d
github-aliceyang <aliyang47@gmail.com>
2023-09-05 15:27:33 -0700

    added the LoFi wireframes for mobile

---
+++ src/MobileHiFi-images.js
@@ -0,0 +1,55 @@
+export const MobileLoFi = [
+    {
+        img:require('./assets/Home Page.jpg')
+    },
+    {
+        img: require('./assets/LogIn Page.jpg')
+    },
+    {
+        img: require('./assets/Detail Page.jpg')
+    },
+    {
+        img: require('./assets/Map.jpg')
+    },
+    {
+        img: require('./assets/Profile.jpg')
+    },
+    {
+        img: require('./assets/More.jpg')
+    },
+
+]
+
+export const MobileHiFi = [
+    {
+        img:require('./assets/homepage.png')
+    },
+    {
+        img: require('./assets/listing.png')
+    },
+    {
+        img: require('./assets/filter.png')
+    },
+    {
+        img: require('./assets/map.png')
+    },
+    {
+        img: require('./assets/details.png')
+    },
+    {
+        img: require('./assets/share.png')
+    },
+    {
+        img: require('./assets/booking.png')
+    },
+    {
+        img: require('./assets/favorite.png')
+    },
+    {
+        img: require('./assets/profile.png')
+    },
+    {
+        img: require('./assets/more.png')
+    },
+    
+]
---
+++ src/components/Handoff.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Handoff() {
+  return (
+      <div className="Handoff">
+
+
+    </div>
+  )
+}
+
+export default Handoff
