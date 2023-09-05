4820c892bd71356912a190dca6a9c81c7893e9e5
github-aliceyang <aliyang47@gmail.com>
2023-09-05 13:03:33 -0700

    created mobile HiFi image array file

---
+++ src/MobileHiFiWireframeImg.js
@@ -0,0 +1,33 @@
+export const MobileHiFi = [
+    {
+        img:require('./assets/homepage.png')
+    },
+    {
+        img: require('./assets/listing.png')
+    },
+    {
+        img: require('./assets/filter.png')
+    },
+    {
+        img: require('./assets/map.png')
+    },
+    {
+        img: require('./assets/details.png')
+    },
+    {
+        img: require('./assets/share.png')
+    },
+    {
+        img: require('./assets/booking.png')
+    },
+    {
+        img: require('./assets/favorite.png')
+    },
+    {
+        img: require('./assets/profile.png')
+    },
+    {
+        img: require('./assets/more.png')
+    },
+    
+]
